f6486da8df2487740f467514a9e8ff71a9459b81
devanb4264 <dbush1@una.edu>
2024-08-29 10:46:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express()
 
 console.log('im on a node server, yo');
 
+//app.use(express.static('./')) 
 app.get('/', function (req, res) {
   res.send('Hello Node from Ex on local dev box')
-  //res.sendFile('index.html');
+  res.sendFile('index.html');
 })
 
 app.listen(3000)
