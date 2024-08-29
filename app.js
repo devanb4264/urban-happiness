6e4a68d5f8e4f995d4f7aaf468d9a2649e4233f7
devanb4264 <dbush1@una.edu>
2024-08-29 13:46:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const app = express()
-
+app.set('view engine', 'ejs')
+app.use(express.static('./public/')) 
 console.log('im on a node server, here is my doge game');
 
-app.use(express.static('./public/')) 
 app.get('/', function (req, res) {
   //res.send('Hello Node from Ex on local dev box')
   res.sendFile('index.html');
   res.sendFile('scripts/script.js');
   res.sendFile('styles/style.css');
+})
+app.get('/ejs', (req, res)=> {
+
+  res.render('index', {
+    myServerVariable : "something from server"
+  });
+
+  //can you get content from client... to console?
 })
 
 app.listen(3000)
