6ea4dc2fd28b51a9668c26f0748735e2acb34a41
devanb4264 <dbush1@una.edu>
2024-08-29 10:53:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 
-console.log('im on a node server, yo');
+console.log('im on a node server, here is my doge game');
 
 app.use(express.static('./')) 
 app.get('/', function (req, res) {
-  res.send('Hello Node from Ex on local dev box')
+  //res.send('Hello Node from Ex on local dev box')
   res.sendFile('index.html');
   res.sendFile('scripts/script.js');
   res.sendFile('styles/style.css');
