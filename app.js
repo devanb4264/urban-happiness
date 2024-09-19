a98e6b42f26827332fa698af5f9cd3d698abd427
devanb4264 <dbush1@una.edu>
2024-09-18 19:40:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -88,7 +88,8 @@ app.get('/insert', async (req,res)=> {
   //connect to db,
   await client.connect();
   //point to the collection 
-  await client.db("deavns-db").collection("whatever-collection").insertOne({ POST: 'hardcoded POST insert '});
+  await client.db("barrys-db").collection("whatever-collection").insertOne({ post: 'hardcoded post insert '});
+  await client.db("barrys-db").collection("whatever-collection").insertOne({ iJustMadeThisUp: 'hardcoded new key '});  
   //insert into it
   res.render('insert');
 
