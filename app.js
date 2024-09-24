36351be6a9635d073c2ece351705a6a241fc991e
devanb4264 <dbush1@una.edu>
2024-09-24 12:38:45 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-require('dotenv').config();
+require('dotenv').config()
 const express = require('express')
 const app = express()
-
-app.set('view engine', 'ejs')
-console.log("I'm on a node server")
-
-app.use(express.static('./public/'))
-
+const bodyParser = require('body-parser')
+const { urlencoded } = require('body-parser')
+const { ObjectId } = require('mongodb')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.URI;
-//console.log(uri);
+
+console.log("I'm on a node server")
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+app.use(express.static('./public/'))
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -56,9 +58,70 @@ app.get('/', function (req, res) {
 })
 
 app.get('/ejs', (req, res)=>{
-    res.render("mongo", {
-      mongoResult: result[0].post
-    });
+    res.render("index", {
+      myServerVariable: "Something from server"
+    })
+})
+
+//CRUD OPS
+app.get('/read', async (req, res)=>{
+
+  console.log('in /mongo');
+  await client.connect();
+  
+  console.log('connected?');
+  // Send a ping to confirm a successful connection
+  
+  let result = await client.db("devans-db").collection("whatever-collection")
+    .find({}).toArray(); 
+  console.log(result); 
+
+  res.render('mongo', {
+    postData : result
+  });
+
+})
+
+app.get('/insert', async (req,res)=> {
+
+  console.log('in /insert');
+  //connect to db,
+  await client.connect();
+  //point to the collection 
+  await client.db("devans-db").collection("whatever-collection").insertOne({ post: 'hardcoded post insert '});
+  //insert into it
+  res.render('insert');
+
+}); 
+
+app.post('/update/:id', async (req,res)=>{
+
+  console.log("req.parms.id: ", req.params.id)
+
+  client.connect;
+  const collection = client.db("devans-db").collection("whatever-collection");
+  let result = await collection.findOneAndUpdate( 
+  {"_id": new ObjectId(req.params.id)}, { $set: {"post": "NEW POST" } }
+)
+.then(result => {
+  console.log(result); 
+  res.redirect('/read');
+})
+}); 
+
+app.post('/delete/:id', async (req,res)=>{
+
+  console.log("req.parms.id: ", req.params.id)
+
+  client.connect; 
+  const collection = client.db("devans-db").collection("whatever-collection");
+  let result = await collection.findOneAndDelete( 
+  {"_id": new ObjectId(req.params.id)})
+
+.then(result => {
+  console.log(result);
+  res.redirect('/read');
+})
 })
 
 app.listen(5000)
