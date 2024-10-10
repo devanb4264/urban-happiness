cfcbdd8a0cb7deba11386e7b7b85e34d2c19d564
devanb4264 <dbush1@una.edu>
2024-10-10 01:35:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
 require('dotenv').config();
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const { ObjectId } = require('mongodb');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const { urlencoded } = require('body-parser')
+const { ObjectId } = require('mongodb')
+
+console.log("I'm on a node server")
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+app.use(express.static('./public/'))
+
+//cheese hash value
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGO_URI;
-const PORT = process.env.PORT || 3000;
+const uri = process.env.URI;
+//console.log(uri);
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
-app.use(express.static('./public/'));
-
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
-
 // Main route to render `index.ejs` with MongoDB data
 app.get('/', async (req, res) => {
     try {
         await client.connect();
-        const result = await client.db("Effective-lamp").collection("Effective-lamp").find({}).toArray();
+        const result = await client.db("devan's-db").collection("whatever-collection").find({}).toArray();
         res.render('index', {
             postData: result
         });
@@ -40,7 +45,7 @@ app.post('/saveConverted', async (req, res) => {
 
     try {
         await client.connect();
-        await client.db("Effective-lamp").collection("Effective-lamp").insertOne({ post: convertedText });
+        await client.db("devan's-db").collection("whatever-collection").insertOne({ post: convertedText });
         res.redirect('/');
     } catch (error) {
         console.error('Error saving post:', error);
@@ -55,7 +60,7 @@ app.post('/update/:id', async (req, res) => {
 
     try {
         await client.connect();
-        await client.db("Effective-lamp").collection("Effective-lamp").findOneAndUpdate(
+        await client.db("devan's-db").collection("whatever-collection").findOneAndUpdate(
             { "_id": new ObjectId(postId) },
             { $set: { "post": newPost } }
         );
@@ -72,7 +77,7 @@ app.post('/delete/:id', async (req, res) => {
 
     try {
         await client.connect();
-        await client.db("Effective-lamp").collection("Effective-lamp").findOneAndDelete({ "_id": new ObjectId(postId) });
+        await client.db("devan's-db").collection("whatever-collection").findOneAndDelete({ "_id": new ObjectId(postId) });
         res.redirect('/');
     } catch (error) {
         console.error('Error deleting post:', error);
