2b66d98069e961fee852d0276477b66a7ee2d97c
devanb4264 <dbush1@una.edu>
2024-10-10 00:55:59 -0500

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,28 +1,65 @@
-//$ jquery land
-$(document).ready(function(){
-  console.log("Top: " + $("img").position().top + " Left: " + $("img").position().left);
-  
-  $("p").position();
- 
-  //make image draggable
-  
-  $("#dogememe").draggable({
-    stop: function () {
-      console.log("stopping???");
+$(document).ready(function() {
+    // Function to convert text to NATO phonetic alphabet
+    function convertToNatoPhonetic(text) {
+        var natoPhonetic = {
+            'A': 'Alpha', 'B': 'Bravo', 'C': 'Charlie', 'D': 'Delta', 'E': 'Echo',
+            'F': 'Foxtrot', 'G': 'Golf', 'H': 'Hotel', 'I': 'India', 'J': 'Juliett',
+            'K': 'Kilo', 'L': 'Lima', 'M': 'Mike', 'N': 'November', 'O': 'Oscar',
+            'P': 'Papa', 'Q': 'Quebec', 'R': 'Romeo', 'S': 'Sierra', 'T': 'Tango',
+            'U': 'Uniform', 'V': 'Victor', 'W': 'Whiskey', 'X': 'X-ray', 'Y': 'Yankee', 'Z': 'Zulu'
+        };
+        // Convert each character to its NATO phonetic equivalent
+        var phoneticText = '';
+        for (var i = 0; i < text.length; i++) {
+            var char = text.charAt(i).toUpperCase();
+            if (natoPhonetic[char]) {
+                phoneticText += natoPhonetic[char] + ' ';
+            } else {
+                phoneticText += char + ' '; 
+            }
+        }
+        return phoneticText.trim(); 
+    }
 
+    // Function to convert text to kebab-case
+    function convertToKebabCase(text) {
+        return text.toLowerCase().replace(/\s+/g, '-');
+    }
 
-      // if( img is far right side  then ) user wins.
+    // Event listener for convert button
+    $('#convertBtn').click(function() {
+        var inputText = $('#textInput').val().trim();
+        if (inputText !== '') {
+            var outputText;
+            var outputType = $('input[name=outputType]:checked').val();
+            switch (outputType) {
+                case 'kebab':
+                    outputText = convertToKebabCase(inputText);
+                    break;
+                default:
+                    outputText = convertToNatoPhonetic(inputText);
+            }
+            $('#outputList').append('<li class="list-group-item">' + outputText + '</li>');
+        }
+    });
 
-      let dogeLeft = $("img").position().left;
+    // Event listener for clear button
+    $('#clearBtn').click(function() {
+        $('#textInput').val('');
+        $('#outputList').empty();
+    });
 
-      if (dogeLeft > 1000) {
-        console.log("we above 1000!", dogeLeft);
+    // Hamburger menu toggle for smaller screens
+    $('.navbar-toggler').click(function() {
+        $('.navbar-collapse').toggleClass('show');
+    });
 
-        $("h1").text("Am full now, much happy!");
-
-      } else {
-        console.log("we NOT above 1000");
-      }
-    },
-  });
+    // Event listener for text input
+    $('#textInput').on('input', function() {
+        if ($(this).val().trim() !== '') {
+            $('#goCrazy').slideDown();
+        } else {
+            $('#goCrazy').slideUp();
+        }
+    });
 });
